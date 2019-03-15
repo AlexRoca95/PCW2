b6057f1c6da6e1315ec18a3e4b110de3175b0f99
pcw <roca-vandesype@hotmail.com>
2019-03-15 12:51:12 +0100

    Login check usuario y redireccion

---
+++ practica02/javascript.js
@@ -0,0 +1,44 @@
+
+
+// Función para hacer el login del usuario comprobando que el usuario y contraseña son correctos
+function loginUsuario(formulario)
+{
+	let xhr = new XMLHttpRequest();  // Creamos el objeto para poder hacer una petición al servidor.
+
+	if(xhr) // Si se ha creado el objeto, se completa la petición
+	{
+		let fd = new FormData(formulario); // Mediante formData podemos encapsular todos los campos de un formulario de HTML
+			url = 'api/sesiones'; 			// Direccion donde estan las sesiones
+
+		xhr.open('POST', url, true);   // Indicamos que queremos enviar datos al servidor
+
+		xhr.onload=function()  // La funcion se lanzara cuando finaliza la peticion y se ha recibido una respuesta.
+		{
+
+			if(xhr.status==200)  // Si el valor devuelto es 200 quiere decir que esta todo correcto.
+			{
+
+				console.log('login Correcto');
+
+				sessionStorage.setItem("login", formulario.login.value);  // Asiganmos el valor del campo de login del formulario al campo de login del sessionStorage
+				sessionStorage.setItem("pwd", formulario.pwd.value);
+
+				// Redireccion cuando se ha hecho el login bien
+				window.location.replace('index.html');   		 	// REPLACE --> Borra la url actual de tal manera que no se puede volver atras al pulsar el boton de ir 
+																	// a la pagina anterior.
+
+				//window.location.href='index.html'; 				// HREF --> Si que se puede ir a la pagina anterior.
+
+			}
+
+
+		};
+
+
+		xhr.send(fd); 			// Enviamos los datos del formulario
+
+		return false;    		// Evita que recarge la pagina cada vez que pulsamos el boton de enviar el formulario
+
+	}
+
+}
