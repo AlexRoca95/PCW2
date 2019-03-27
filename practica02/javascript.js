36a05a2b573deb26c53ea876871dc0c1d5dccc2f
pcw <roca-vandesype@hotmail.com>
2019-03-27 16:47:54 +0100

    Datos fotos index e intento favoritas

--- practica02/javascript.js
+++ practica02/javascript.js
@@ -23,6 +23,9 @@ function loginUsuario(formulario)
 				sessionStorage.setItem("login", formulario.login.value);  // Asiganmos el valor del campo de login del formulario al campo de usuario del sessionStorage
 				sessionStorage.setItem("pwd", formulario.pwd.value);
 
+				sessionStorage['usuario'] = xhr.responseText; // Almacenamos en la variable usuario toda la respuesta
+
+
 				// Redireccion cuando se ha hecho el login bien
 				window.location.replace('index.html');   		 	// REPLACE --> Borra la url actual de tal manera que no se puede volver atras al pulsar el boton de ir 
 																	// a la pagina anterior.
@@ -48,39 +51,42 @@ function fotosMejorValoradas()
 {
 	// Peticion GET
 	let xhr = new XMLHttpRequest();
-		url = 'api/fotos';
 
+	url = 'api/fotos/megusta';
 	xhr.open('GET', url, true);
 
 	xhr.onload= function(){
 
 		let r = JSON.parse(xhr.responseText);
 
+		//console.log(r);
+
 		html = '';
 
 		/* Recorremos hasta 6 fotos para mostar en la primera pagina de index*/
 		for (let i = 0; i < 6; i++)  
 		{
+
 			html += '<article>';
 
 				html += '<div class="contenedor">';
 
 					html += '<div class="autorBox">';
 
-						html += '<p><a class="enlaces" title="Buscar por autor" href="buscar.html">Roxo95</a> <b>|</b> <a class="enlaces" title="Buscar por etiquetas" href="buscar.html">#hackaton</a> <a class="enlaces" title="Buscar por etiquetas" href="buscar.html">#2018</a></p>';
+						html += '<p><a class="enlaces" title="Buscar por autor" href="buscar.html">' + r.FILAS[i].login + '</a> <b>|</b> <a class="enlaces" title="Buscar por etiquetas" href="buscar.html">#hackaton</a> <a class="enlaces" title="Buscar por etiquetas" href="buscar.html">#2018</a></p>';
 
 					html += '</div>';
 
 					html += '<a title="Ver foto" href="foto.html">';
 
-						html += '<img src="fotos/' + r.FILAS[i].fichero + '" alt="Foto random de muestra" class="imagIndex" >'; 
+						html += '<img src="fotos/' + r.FILAS[i].fichero + '" alt="'+ r.FILAS[i].descripcion +'" class="imagIndex" >'; 
 
 					html += '</a>';
 
 					html += '<div class="textoImg">';
 
 						html += '<h4>' + r.FILAS[i].titulo + '</h4>';
-						html += '<p><span class="icon-thumbs-up"></span>15 <span class="icon-heart-empty"></span>5 <span class="icon-comment-empty"></span>3</p>';
+						html += '<p><span class="icon-thumbs-up"></span>' + r.FILAS[i].nmegusta + ' <span class="icon-heart-empty"></span>' + r.FILAS[i].nfavorita + ' <span class="icon-comment-empty"></span>' + r.FILAS[i].ncomentarios + '</p>';
 
 					html += '</div>';
 				html += '</div>';
@@ -93,6 +99,58 @@ function fotosMejorValoradas()
 
 
 	};
+
+	xhr.send();  // Envia la petición que hemos hecho al servidor
+
+
+
+}
+
+
+
+// Muestra las fotos favoritas del usuario
+function fotosFavoritas()
+{
+	// Peticion GET
+	let xhr = new XMLHttpRequest();
+		url = 'api/usuarios/'; 
+
+	let usu = JSON.parse(sessionStorage['usuario']);
+
+	// Completamos la url con la info que falta
+	url += usu.login + '/favoritas';  // Favoritas del usuario
+
+	xhr.open('GET', url, true);
+
+	xhr.onload = function()
+	{
+		let r = JSON.parse(xhr.responseText);
+
+		// Si todo ha ido bien
+		if(r.RESULTADO == 'OK')
+		{
+			let html = '';
+
+			console.log(r.FILAS);
+
+			for (i=0; i<r.FILAS[i]; i++)
+			{
+
+				html += '<article>';
+
+					html += '<p>HOla cars</p>';
+					
+				html += '</article>';
+
+			}
+
+			document.querySelector('.preview').innerHTML=html;
+
+		}
+
+	};
+
+	xhr.setRequestHeader('Authorization', usu.login + ':' + usu.token);  // Autorizacion necesaria para acceder a las fotos favoritas
 
 	xhr.send();  // Envia la petición que hemos hecho al servidor
 
